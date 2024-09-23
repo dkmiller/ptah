ca6248d89d689f96d5354d363387da5bba2a5ed9
Dan Miller <dan.miller@airbnb.com>
2024-09-22 19:34:46 -0700

    Increase test coverage (#13)
    * Increase test coverage
    
    * Add code coverage
    
    * Add pytest-cov + bump pytest
    
    * install -e
    
    * singleton --> top python
    
    * add permissions
    
    * codecov uses oidc
    
    * Add permissions: https://stackoverflow.com/a/74167257/2543689

--- ptah/clients/filesystem.py
+++ ptah/clients/filesystem.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 from ptah.models import PROJECT_FILE
 
@@ -10,8 +11,9 @@ class Filesystem:
         """
         return Path(__file__).parents[1].resolve().absolute()
 
-    def project_path(self) -> Path:
-        for candidate in [Path.cwd()] + list(Path.cwd().parents):
+    def project_path(self, location: Optional[Path] = None) -> Path:
+        location = location or Path.cwd()
+        for candidate in [location] + list(location.parents):
             rv = candidate / PROJECT_FILE
             if rv.is_file():
                 return rv
--- tests/clients/test_filesystem.py
+++ tests/clients/test_filesystem.py
@@ -1,7 +1,44 @@
+from uuid import uuid4
+
+import pytest
+
 from ptah.clients import Filesystem, get
+from ptah.models import PACKAGE_NAME
 
 
 def test_package_root_contains_clients():
     filesystem = get(Filesystem)
     clients = filesystem.package_root() / "clients"
     assert clients.is_dir()
+
+
+def test_pyproject():
+    filesystem = get(Filesystem)
+    pyproject = filesystem.pyproject()
+    assert pyproject.is_file()
+    assert PACKAGE_NAME in pyproject.read_text()
+
+
+def test_project_path_when_none_exists():
+    filesystem = get(Filesystem)
+
+    with pytest.raises(RuntimeError) as exc_info:
+        filesystem.project_path()
+
+    assert "Could not find project file" in exc_info.value.args[0]
+
+
+@pytest.mark.parametrize("depth", [1, 5, 10])
+def test_project_path_in_subdirectory(depth, tmp_path):
+    filesystem = get(Filesystem)
+
+    project_path = tmp_path / "ptah.yml"
+    project_path.touch()
+    location = tmp_path
+
+    for _ in range(depth):
+        location = location / str(uuid4())[:8]
+
+    location.mkdir(parents=True)
+
+    assert filesystem.project_path(location) == project_path
