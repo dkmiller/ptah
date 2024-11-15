23717d94ca447fd7ab5e01c9ab0c9c4654526a21
Dan Miller <daniel.keegan.miller@gmail.com>
2024-11-14 20:19:46 -0800

    Add a test

--- tests/clients/test_project.py
+++ tests/clients/test_project.py
@@ -12,3 +12,17 @@ kind:
     project = client.load(path)
     assert isinstance(project, Model)
     assert project.kind.name == "some-name"
+
+
+def test_project_load_api_server(tmp_path):
+    path = tmp_path / "ptah.yml"
+    path.write_text("""
+kind:
+  name: some-name
+api_server:
+  port: 9263
+    """)
+    client = get(Project)
+    project = client.load(path)
+    assert isinstance(project, Model)
+    assert project.api_server.port == 9263
