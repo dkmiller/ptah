b923588a0809d58406adc4c6585756a57f13dbb4
Dan Miller <dan.miller@airbnb.com>
2024-12-14 12:10:51 -0800

    Kind config + MySQL example (#32)
    * WIP: Kind config + MySQL example
    
    * Close to ready
    
    * Add some more tests

--- tests/clients/test_kind.py
+++ tests/clients/test_kind.py
@@ -28,3 +28,18 @@ def test_create(tmp_cwd):
     kind.create()
 
     assert name in kind.shell.call_args.args
+
+
+@pytest.mark.parametrize("in_project", ["project-with-kind-config"], indirect=True)
+def test_kind_path(in_project):
+    kind = get(Kind)
+    path = kind.path()
+
+    assert path
+    assert path.name == "kind.yml"
+
+
+@pytest.mark.parametrize("in_project", ["project-with-kind-config"], indirect=True)
+def test_cluster_name(in_project):
+    kind = get(Kind)
+    assert kind.cluster_name() == "some-kind-cluster-name"
