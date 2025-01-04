195982ee772944c56b96476f6a10672156f6443a
Dan Miller <daniel.keegan.miller@gmail.com>
2025-01-04 11:30:55 -0800

    Test the CLI

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,7 +1,19 @@
+import re
+
 from click import BaseCommand
+from typer.testing import CliRunner
+
+from ptah.cli import app
 
 
 def test_cli_click_object():
     from ptah.cli import ptah
 
     assert isinstance(ptah, BaseCommand)
+
+
+def test_cli_version():
+    runner = CliRunner()
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
+    assert re.match(r"\d+\.\d+\.\d+", result.stdout)
