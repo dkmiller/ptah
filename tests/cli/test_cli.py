aad981baa65b725df28c9d0b0a2371237d52cf6c
Dan Miller <daniel.keegan.miller@gmail.com>
2025-06-01 08:40:30 -0700

    Fix deprecation error against latest click (#54)

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,6 +1,6 @@
 import re
 
-from click import BaseCommand
+from click import Command
 from typer.testing import CliRunner
 
 from ptah.cli import app
@@ -9,7 +9,7 @@ from ptah.cli import app
 def test_cli_click_object():
     from ptah.cli import ptah
 
-    assert isinstance(ptah, BaseCommand)
+    assert isinstance(ptah, Command)
 
 
 def test_cli_version():
